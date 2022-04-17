9745f55e9058e2bb9d501da1be02ad5ed0dda006
ibrahim <yzcibrahim@gmail.com>
2022-04-17 11:41:59 +0300

    Add project files.

---
+++ KisiTakipMvc/Controllers/KisiController.cs
@@ -0,0 +1,22 @@
+﻿using KisiTakipMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KisiTakipMvc.Controllers
+{
+    public class KisiController : Controller
+    {
+        public IActionResult Index()
+        {
+            //KisiViewModel model = new KisiViewModel() { Id = 1, Name = "ibrahim", Surname = "Yazıcı" };
+            List<KisiViewModel> model = new List<KisiViewModel>();
+            model.Add(new KisiViewModel() { Id = 1, Name = "İbrahim", Surname = "Yazıcı" });
+            model.Add(new KisiViewModel() { Id = 2, Name = "Cem", Surname = "Akdemir" });
+            model.Add(new KisiViewModel() { Id = 3, Name = "Elif", Surname = "Müftioğlu" });
+            return View(model);
+        }
+    }
+}
---
+++ KisiTakipMvc/Models/KisiViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KisiTakipMvc.Models
+{
+    public class KisiViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
