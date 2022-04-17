409ef5e2c24471e9e24940a0c055e1c7e33e979c
ibrahim <yzcibrahim@gmail.com>
2022-04-17 13:52:57 +0300

    end of week

--- KisiTakipMvc/Controllers/KisiController.cs
+++ KisiTakipMvc/Controllers/KisiController.cs
@@ -9,14 +9,80 @@ namespace KisiTakipMvc.Controllers
 {
     public class KisiController : Controller
     {
+        static List<KisiViewModel> kisiler;
+
+        [HttpGet]
         public IActionResult Index()
         {
+            if (kisiler == null)
+            {
+                kisiler = new List<KisiViewModel>();
+                kisiler.Add(new KisiViewModel() { Id = 1, Name = "İbrahim", Surname = "Yazıcı" });
+                kisiler.Add(new KisiViewModel() { Id = 2, Name = "Cem", Surname = "Akdemir" });
+                kisiler.Add(new KisiViewModel() { Id = 3, Name = "Elif", Surname = "Müftüoğlu" });
+            }
+
             //KisiViewModel model = new KisiViewModel() { Id = 1, Name = "ibrahim", Surname = "Yazıcı" };
-            List<KisiViewModel> model = new List<KisiViewModel>();
-            model.Add(new KisiViewModel() { Id = 1, Name = "İbrahim", Surname = "Yazıcı" });
-            model.Add(new KisiViewModel() { Id = 2, Name = "Cem", Surname = "Akdemir" });
-            model.Add(new KisiViewModel() { Id = 3, Name = "Elif", Surname = "Müftioğlu" });
+           
+            return View(kisiler);
+        }
+
+        public IActionResult Details(int id)
+        {
+            KisiViewModel model = kisiler.FirstOrDefault(c => c.Id == id);
             return View(model);
         }
+
+        public IActionResult Create(int? id)
+        {
+            if (id.HasValue)
+            {
+                KisiViewModel model = kisiler.FirstOrDefault(c => c.Id == id);
+                return View(model);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create(KisiViewModel kisi)
+        {
+            int maxId = kisiler.Max(c => c.Id) + 1;
+            //KisiViewModel yeniKisi = new KisiViewModel() { Id = maxId, Name = Name, Surname = Surname };
+            //  KisiViewModel yeni = new KisiViewModel() { Id = 5, Name = Request.Form["Name"], Surname = Request.Form["Surname"] };
+
+            if (kisi.Id <= 0)
+            {
+                kisi.Id = maxId;
+                kisiler.Add(kisi);
+            }
+            else
+            {
+                KisiViewModel updateKisi = kisiler.First(c => c.Id == kisi.Id);
+                updateKisi.Name = kisi.Name;
+                updateKisi.Surname = kisi.Surname;
+
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int id)
+        {
+            KisiViewModel model = kisiler.FirstOrDefault(c => c.Id == id);
+            return View(model);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            //var silinecek = kisiler.FirstOrDefault(c => c.Id == id);
+            //kisiler.Remove(silinecek);
+
+            kisiler = kisiler.Where(c => c.Id != id).ToList();
+            return RedirectToAction(nameof(Index)); 
+
+        }
+
     }
 }
--- KisiTakipMvc/Models/KisiViewModel.cs
+++ KisiTakipMvc/Models/KisiViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,13 @@ namespace KisiTakipMvc.Models
 {
     public class KisiViewModel
     {
+        
         public int Id { get; set; }
+        
+        [DisplayName("Ad")]
         public string Name { get; set; }
+
+        [DisplayName("Soyad")]
         public string Surname { get; set; }
     }
 }
